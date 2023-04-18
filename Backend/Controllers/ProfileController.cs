acb2837792f9fd89a3a45c64d557003e3d7f8d85
Яковенко Дима <dmityakovenko@gmail.com>
2023-04-18 14:35:23 +0300

    a

--- Backend/Controllers/ProfileController.cs
+++ Backend/Controllers/ProfileController.cs
@@ -4,6 +4,7 @@ using Backend.Core.Metods;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Backend.Metods;
+using Backend.Model;
 
 namespace Backend.Controllers
 {
@@ -21,7 +22,7 @@ namespace Backend.Controllers
 
         [Authorize(Roles = "admin")]
         [HttpGet]
-        [Route("get")]
+        [Route("getAll")]
         public async Task<ActionResult<List<ProfileGetDTO>>> Get()
         {
             List<ProfileGetDTO> res = await service.GetProfiles();
@@ -32,7 +33,7 @@ namespace Backend.Controllers
             return Ok(res);
         }
 
-        [Authorize]
+        [Authorize(Roles = "admin")]
         [HttpGet]
         [Route("get/{id}")]
         public async Task<ActionResult<ProfileGetDTO>> Get(int id)
@@ -49,6 +50,21 @@ namespace Backend.Controllers
             }
             return Ok(res);
         }
+
+        [Authorize(Roles = "user")]
+        [HttpGet]
+        [Route("get")]
+        public async Task<ActionResult<ProfileGetDTO>> GetUser()
+        {
+            var user = CurrentUser.Get(HttpContext);
+            ProfileGetDTO res = await service.GetProfile(user.ProfileId);
+            if (res == null)
+            {
+                return NotFound();
+            }
+            return Ok(res);
+        }
+
         [Authorize]
         [HttpDelete]
         [Route("delete/{id}")]
@@ -66,23 +82,51 @@ namespace Backend.Controllers
             }
             return Ok(res);
         }
-        [Authorize]
+        [Authorize(Roles = "admin")]
         [HttpPatch]
-        [Route("patch")]
-        public async Task<ActionResult<ProfileGetDTO>> Patch(ProfilePatchDTO profilePatchDTO)
+        [Route("patch/{id}")]
+        public async Task<ActionResult<ProfileGetDTO>> Patch(ProfilePatch profilePatchDTO, int id)
         {
-            var user = CurrentUser.Get(HttpContext);
-            if (user.Role != "admin" && user.ProfileId != profilePatchDTO.ProfileId)
-            {
-                return NotFound();
-            }
-            ProfileGetDTO res = await service.ProfilePatch(profilePatchDTO);
+            
+            ProfileGetDTO res = await service.ProfilePatch(new ProfilePatchDTO() 
+            { 
+                Email = profilePatchDTO.Email, 
+                Login = profilePatchDTO.Login, 
+                Name = profilePatchDTO.Name, 
+                PhoneNumber = profilePatchDTO.PhoneNumber,
+                ProfileId = id,
+                Role = profilePatchDTO.Role,
+            });
             if (res == null)
             {
                 return NotFound();
             }
             return Ok(res);
         }
+
+        [Authorize(Roles = "user")]
+        [HttpPatch]
+        [Route("patch")]
+        public async Task<ActionResult<ProfileGetDTO>> PatchUser(ProfilePatch profilePatchDTO)
+        {
+
+            var user = CurrentUser.Get(HttpContext);
+            ProfileGetDTO res = await service.ProfilePatch(new ProfilePatchDTO()
+            {
+                Email = profilePatchDTO.Email,
+                Login = profilePatchDTO.Login,
+                Name = profilePatchDTO.Name,
+                PhoneNumber = profilePatchDTO.PhoneNumber,
+                ProfileId = user.ProfileId,
+                Role = profilePatchDTO.Role,
+            });
+            if (res == null)
+            {
+                return NotFound();
+            }
+            return Ok(res);
+        }
+
 
         [Authorize(Roles = "user")]
         [HttpPatch]
---
+++ Backend/Model/ProfilePatch.cs
@@ -0,0 +1,11 @@
+﻿namespace Backend.Model
+{
+    public class ProfilePatch
+    {
+        public string Name { get; set; }
+        public string Login { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Role { get; set; }
+    }
+}
